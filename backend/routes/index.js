f35a928d0def2e9a78d29fef913f8039ca19fc69
Alok <aloku901@gmail.com>
2024-04-06 22:44:19 +0530

    Add Acount Route and Schema

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const user = require("./user");
+const accountRouter = require("./account");
+
 const router = express.Router();
 
 router.use("/user", user);
+router.use("/account", accountRouter);
 
 module.exports = router;
